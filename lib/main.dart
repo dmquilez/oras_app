6eeb0f106131813ea17d37b0eea39c3a959f8d2b
dmquilez <hola@dmq.me>
2021-11-20 04:19:24 +0100

    Portrait mode v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'navigation/navigate.dart';
 import 'utils/constants.dart';
-import 'package:flutter/services.dart';
-
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((_) {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_) {
     runApp(MyApp());
   });
+
 
 }
 
